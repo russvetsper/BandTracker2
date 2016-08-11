4af7eceb30de540c0e67796d6d264ffcd6b6651f
russvetsper <russvetsper@gmail.com>
2016-08-11 12:47:42 -0700

    pass test7 band empty database

--- Ojects/Band.cs
+++ Ojects/Band.cs
@@ -36,5 +36,35 @@ namespace BandTracker
       cmd.ExecuteNonQuery();
       conn.Close();
     }
+
+    public static List<Band> GetAll()
+   {
+     List<Band> allBands = new List<Band>{};
+
+     SqlConnection conn = DB.Connection();
+     conn.Open();
+
+     SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+     SqlDataReader rdr = cmd.ExecuteReader();
+
+     while(rdr.Read())
+     {
+       int bandId = rdr.GetInt32(0);
+       string bandName = rdr.GetString(1);
+       Band newBand = new Band(bandName, bandId);
+       allBands.Add(newBand);
+     }
+
+     if (rdr != null)
+     {
+       rdr.Close();
+     }
+     if (conn != null)
+     {
+       conn.Close();
+     }
+
+     return allBands;
+   }
   }
 }
--- Tests/BandTest.cs
+++ Tests/BandTest.cs
@@ -13,6 +13,17 @@ namespace BandTracker
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
     }
 
+    [Fact]
+   public void Test7_EmptyDatabase()
+   {
+     //Arrange, Act
+     int result = Band.GetAll().Count;
+
+     //Assert
+     Assert.Equal(0, result);
+   }
+
+
 
     public void Dispose()
     {
